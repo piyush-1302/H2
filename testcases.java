adc44a56344e4d65315085d10f7109c39d75140b
Prashant <prashant.singh@paysafe.com>
2020-10-12 20:49:23 +0530

    hew bug-1

---
+++ testcases.java
@@ -0,0 +1,19 @@
+
+ne
+edit
+play_arrow
+
+brightness_4
+// Java code to illustrate 
+// System.out.println(); 
+  
+  import java.io.*; 
+    
+    class GFG { 
+        public static void main(String[] args) 
+	    { 
+	            System.out.println("Welcome"); 
+		            System.out.println("To"); 
+			            System.out.println("GeeksforGeeks"); 
+				        } 
+					} 
